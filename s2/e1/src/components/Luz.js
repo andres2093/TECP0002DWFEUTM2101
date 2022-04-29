46401ff81255afdc5026d71ce73a8f012f84ad8f
Andres Ramirez <andres200493@gmail.com>
2022-04-28 19:47:49 -0500

    S2 -> E1

--- s2/e1/src/components/Luz.js
+++ s2/e1/src/components/Luz.js
@@ -1,8 +1,27 @@
-const Luz = () => {
-  return (
-    <div className="luz">
-    </div>
-  );
-}
+// const Luz = () => {
+//   return (
+//     <div className="luz">
+//     </div>
+//   );
+// }
+
+// stateful -> Componente clase
+import React from 'react';
+
+class Luz extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: 'pink'
+    }
+  }
+
+   render() {
+      return (
+        <div style={{ backgroundColor: this.state.color }} className="luz">
+        </div>
+      );
+   }
+};
 
 export default Luz;
--- s2/e1/src/components/LuzMercurial.js
+++ s2/e1/src/components/LuzMercurial.js
@@ -1,8 +1,27 @@
-const LuzMercurial = () => {
-  return (
-    <div className="luzMercurial">
-    </div>
-  );
-}
+// const LuzMercurial = () => {
+//   return (
+//     <div className="luzMercurial">
+//     </div>
+//   );
+// }
+
+// stateful -> Componente clase
+import React from 'react';
+
+class LuzMercurial extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: 'pink'
+    }
+  }
+
+   render() {
+      return (
+        <div style={{ backgroundColor: this.state.color }} className="luzMercurial">
+        </div>
+      );
+   }
+};
 
 export default LuzMercurial;
