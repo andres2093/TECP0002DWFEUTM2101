6c0b883ec31e94ba50fb5f0b5eb05b6fbb3924c6
Andres Ramirez <andres200493@gmail.com>
2022-05-03 19:59:25 -0500

    S3 -> E1

---
+++ s3/ejemplo1/src/components/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Boton from "./Boton";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contador: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      contador: this.state.contador + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className="margen">
+        {/* { this.state.contador } */}
+
+        <Boton
+          texto={ this.state.contador }
+          handleClick= { this.handleClick }
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
