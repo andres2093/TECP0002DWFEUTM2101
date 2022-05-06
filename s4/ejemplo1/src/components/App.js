8bf7584a6b681d2c94f84c0ee28586e4ff087228
Andres Ramirez <andres200493@gmail.com>
2022-05-05 20:42:39 -0500

    S4 -> R1

--- s4/ejemplo1/src/components/App.js
+++ s4/ejemplo1/src/components/App.js
@@ -1,21 +1,34 @@
-import React from "react";
-import Boton from "./Boton";
+import React from 'react';
+import Boton from './Boton';
 
 const App = () => {
-  const [contador, setContador] = React.useState(0)
-
-  const handleClick = () => setContador(contador + 1)
+  const [contador, setContador] = React.useState(0);
+  const [texto, setTexto] = React.useState("Cero");
+  
+  const handleConvertir = (numero, texto) => {
+    setContador(numero)
+    setTexto(texto)
+  }
 
   return (
-    <div className="margen">
-      {/* <button onClick={handleClick}>
-        {contador}
-      </button> */}
-      <Boton
-        texto={contador}
-        handleClick={handleClick}/>
-    </div>
-  )
-}
+  <div className="margen">
+    {contador} - {texto}
+    <br/>
+    <Boton            
+      texto={'1'}            
+      handleClick={() => handleConvertir(1, 'uno')}
+    />
+    <Boton            
+      texto={'2'}            
+      handleClick={() => handleConvertir(2, 'dos')}
+    />
+    <Boton            
+      texto={'3'}            
+      handleClick={() => handleConvertir(3, 'tres')}
+    />
+  </div>   
+  );
+
+};
 
 export default App;
