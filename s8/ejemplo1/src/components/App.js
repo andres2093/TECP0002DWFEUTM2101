c31cda3c803630548a27b1fe3d530ba31f8b7213
Andres Ramirez <andres200493@gmail.com>
2022-05-19 19:49:00 -0500

    S8 -> E1

---
+++ s8/ejemplo1/src/components/App.js
@@ -0,0 +1,46 @@
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+
+const App = () => {
+  return (
+    <Container className="container">
+      <Grid container spacing={3}>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <div className='gridItem'>
+            Grid 1
+          </div>
+        </Grid>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <div className='gridItem'>
+            Grid 2
+          </div>
+        </Grid>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <div className='gridItem'>
+            Grid 3
+          </div>
+        </Grid>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <div className='gridItem'>
+            Grid 4
+          </div>
+        </Grid>
+
+        <Grid item sm={6} xs={12}>
+          <div className='gridItem'>
+            Grid item con container
+            <Grid container>
+              <Grid item lg={3} md={4} sm={6} xs={12}>
+                <div className='anidado'>
+                  Grid anidado
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default App
